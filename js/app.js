6bc15a4bf3c57d9e3cdca8109a2bc58909bd3655
tektechnologies <info@tektechnologies.com>
2022-03-17 11:18:57 -0500

    add js file set up

---
+++ js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+console.log('app.js is connected.');
+
+
+//comment
+
+/**
+ * comment
+ * enter
+ * another star
+ * multi line comment
+ * 
+ */
+
+
+//ask the user their name
+
+
+
+//ask what time is it? 
+
+
+//have some logic to respond to the user.
+
+
+
+
+//out put the users response to the page. 
+
+
